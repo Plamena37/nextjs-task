46649b8345d8230f6f62bc5fcc9f4970d3849078
Plamena <yomena37@gmail.com>
2024-04-25 20:15:07 +0300

    Add card component

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,33 @@
-// The Card to be exported goes here
+import Image from "next/image";
+
+import {
+  StyledContainer,
+  StyledTitle,
+  StyledDescription,
+  StyledTextContainer,
+  StyledImageContainer,
+} from "./elements";
+
+export const Card = ({ card, ...props }) => {
+  const { image, title, description, bgColor, width, isSelected } = card;
+
+  return (
+    <StyledContainer
+      bgColor={bgColor}
+      isSelected={isSelected}
+      width={width}
+      {...props}
+    >
+      <StyledImageContainer>
+        <Image alt="Icon" {...image} />
+      </StyledImageContainer>
+
+      <StyledTextContainer>
+        <StyledTitle isSelected={isSelected}>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
+      </StyledTextContainer>
+    </StyledContainer>
+  );
+};
+
+export default Card;
--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,1 +1,59 @@
-// Styled elements for the Card go here
+// import { Card } from "../../collections/Card/Card";
+import styled from "styled-components";
+import {
+  SectionContainer,
+  SectionBigHeading,
+  SectionSubheading,
+} from "~/components";
+
+export const StyledContainer = styled(
+  ({ height, bgColor, width, isSelected, ...props }) => (
+    <SectionContainer {...props} />
+  )
+)`
+  padding: 1rem 1rem 0.3rem;
+  align-items: center;
+  background: ${({ bgColor }) => bgColor};
+  width: ${({ width }) => width};
+  margin-left: 0;
+  border-radius: 10px;
+  border: ${({ isSelected, theme }) =>
+    isSelected ? `3px solid ${theme.main}` : "none"};
+
+  @media (max-width: 1024px) {
+    width: 80%;
+  }
+`;
+
+export const StyledTitle = styled((props) => <SectionBigHeading {...props} />)`
+  display: inline;
+  margin: 0;
+  font-size: 1.3rem;
+  line-height: 1.2rem;
+  border-bottom: ${({ isSelected, theme }) =>
+    isSelected ? `2px solid ${theme.main}` : "none"};
+
+  color: ${({ isSelected, theme }) => (isSelected ? theme.main : theme.black)};
+
+  ${({ titleStyleProps }) => titleStyleProps}
+`;
+
+export const StyledDescription = styled((props) => (
+  <SectionSubheading {...props} />
+))`
+  margin: 0;
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
+  color: ${({ theme }) => theme.black};
+  width: 80%;
+  font-family: sans-serif;
+`;
+
+export const StyledImageContainer = styled(({ ...props }) => (
+  <div {...props} />
+))`
+  padding: 2rem;
+`;
